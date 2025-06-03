e9e5b15dc8a95fa7d99e7c78b34a75275afee519
TOquation <oparajitee4@gmail.com>
2025-06-03 21:48:15 +0100

    adjustments on next buttons

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,9 @@ export default {
   ],
   theme: {
   	extend: {
+		screens: {
+			'xsm': '375px',
+		},
   		colors: {
   			background: 'hsl(var(--background))',
   			foreground: 'hsl(var(--foreground))',
